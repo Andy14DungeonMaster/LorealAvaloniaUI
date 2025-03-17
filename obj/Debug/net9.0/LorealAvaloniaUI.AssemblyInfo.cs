e8a222eb622fdb743036b94f9e6b54437b520629
Alekhya NANDINA <alekhya.nandina@loreal.com>
2025-03-17 09:44:12 -0400

    Commit changes on latest UI

--- obj/Debug/net9.0/LorealAvaloniaUI.AssemblyInfo.cs
+++ obj/Debug/net9.0/LorealAvaloniaUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LorealAvaloniaUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61652fb7eb9a2b3f90c1456bedc04df282d4f300")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b26c66a02cd2f46b1a52c6166bb5875050853ddc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LorealAvaloniaUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LorealAvaloniaUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
